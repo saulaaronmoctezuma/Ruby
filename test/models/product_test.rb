1bcfce747777b0d595c8bb4a17c53dc611b8f4c4
Sam Ruby <rubys@intertwingly.net>
2017-07-06 18:23:31 -0500

    Scaffold Depot

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -68,6 +68,18 @@ assert product.invalid?
 assert_equal [I18n.translate('errors.messages.taken')],
 product.errors[:title]
 end
+
+
+
+test "product is not valid without a unique title - i18n" do
+product = Product.new(title: products(:ruby).title,
+description: "yyy",
+price: 1,
+image_url: "fred.gif")
+assert product.invalid?
+assert_equal [I18n.translate('errors.messages.taken')],
+product.errors[:title]
+end
 end
 
 
